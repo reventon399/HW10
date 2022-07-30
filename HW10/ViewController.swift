c6b23f394a80da1d02f5a7442b94283bb92b60f7
Alexey <alexeylosev1501@gmail.com>
2022-07-30 11:39:38 +0500

    Add change BG color method.

--- HW10/ViewController.swift
+++ HW10/ViewController.swift
@@ -13,7 +13,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    func changeBackgroundColor() {
+        view.backgroundColor = .systemRed
+    }
 
 }
 
