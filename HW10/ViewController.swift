3afbe9a251724f1b61d667f140a940ccf0354a54
Alexey <alexeylosev1501@gmail.com>
2022-07-30 11:41:01 +0500

    Cleaned code in ViewController.

--- HW10/ViewController.swift
+++ HW10/ViewController.swift
@@ -1,19 +1,9 @@
-//
-//  ViewController.swift
-//  HW10
-//
-//  Created by Алексей Лосев on 30.07.2022.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
